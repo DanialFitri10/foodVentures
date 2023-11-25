7ccc498c085a892c9fb32e8f43dfc97b05f94287
caras13 <carasim80@gmail.com>
2023-11-26 04:51:17 +0800

    implemented forget password and back to login after password reset confirmation

--- public/js/forgetPassword.js
+++ public/js/forgetPassword.js
@@ -1,12 +1,46 @@
-// forge_password.js
-document.getElementById('forgetPasswordForm').addEventListener('submit', function(event) {
-    event.preventDefault();
+// forgetPassword.js
+function forgetPassword() {
     const email = document.getElementById('email').value;
 
-    // Send the email to the server to initiate the password reset process
-    // You'll need server-side code to handle this request, such as sending an email with a unique link to reset the password.
-    // This example only handles the client-side form submission.
+    // sends the email to the server to initiate the password reset process
+    // this code only handles the client-side form submission.
 
     // Redirect the user to a page indicating that an email has been sent for password reset.
     window.location.href = 'password_reset_sent.html';
-});
+}
+
+// function sendResetEmail() {
+//     const email = document.getElementById('email').value;
+
+//     // Basic email validation
+//     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+//     if (!emailRegex.test(email)) {
+//         document.getElementById('emailError').textContent = 'Please enter a valid email address.';
+//         return;
+//     }
+
+//     // Reset error message if email is valid
+//     document.getElementById('emailError').textContent = '';
+
+//     // Send email to server for password reset
+//     fetch('/resetPassword', {
+//         method: 'POST',
+//         headers: {
+//             'Content-Type': 'application/json',
+//         },
+//         body: JSON.stringify({ email }),
+//     })
+//         .then(response => {
+//             if (response.ok) {
+//                 alert('Password reset email sent!');
+//             } else {
+//                 alert('Failed to send reset email.');
+//             }
+//         })
+//         .catch(error => {
+//             console.error('Error:', error);
+//         });
+// }
+
+
+
