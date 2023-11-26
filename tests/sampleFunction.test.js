0284cf4102d1207f7e31aa7142546b0a69f40462
DanialFitri10 <danskaters@gmail.com>
2023-11-27 05:14:38 +0800

    Testing phase for adding blog

--- tests/sampleFunction.test.js
+++ tests/sampleFunction.test.js
@@ -1,7 +1,7 @@
 const { describe, it, beforeEach, afterEach } = require('mocha');
 const { expect } = require('chai');
 const fs = require('fs').promises;
-const { addResource } = require('../utils/ResourceUtil')
+const { addResource } = require('../utils/ResourceUtil');
 const { register } = require('../utils/UserUtil');
 const { User } = require('../models/User');
 
@@ -47,5 +47,62 @@ describe('User Registration Tests', () => {
     });
 });
 
+describe('Add Resource Tests', () => {
+    const testResources = [
+        // Define test resources as needed
+    ];
 
+    beforeEach(() => {
+        // Reset or prepare any necessary state before each test
+    });
 
+    // Test successful resource addition
+    it('Successful Add Resource Test', async () => {
+        const req = {
+            body: {
+                name: 'Test Resource',
+                location: 'Test Location',
+                description: 'Test Description',
+                rating: 5,
+                owner: 'Test Owner',
+            },
+        };
+        const res = {
+            status: (code) => ({ json: (data) => ({ code, data }) }),
+        };
+
+        // Call the addResource function
+        const result = await addResource(req, res);
+
+        // Assert that the resource addition was successful
+        expect(result.code).to.equal(201);
+        expect(result.data).to.be.an('array'); // Adjust this based on your actual response structure
+
+        // You can further check if the new resource is added to your system, e.g., by querying the database or checking an in-memory array
+    });
+
+    // Test unsuccessful resource addition
+    it('Unsuccessful Add Resource Empty Test', async () => {
+        const req = {
+            body: {
+                name: '',
+                location: 'Test Location',
+                description: 'Test Description',
+                rating: 5,
+                owner: 'Test Owner',
+            },
+        };
+        const res = {
+            status: (code) => ({ json: (data) => ({ code, data }) }),
+        };
+
+        // Call the addResource function
+        const result = await addResource(req, res);
+
+        // Assert that the resource addition failed
+        expect(result.code).to.equal(400);
+        expect(result.data.message).to.equal('Invalid data. Resource addition failed.');
+
+        // You can further check if the new resource is not added to your system
+    });
+});
